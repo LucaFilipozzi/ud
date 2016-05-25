39b8e5be3610af01790d8f05a57a41641b0f58cb
Stéphane Blondon <stephane.blondon@gmail.com>
2016-05-25 02:28:59 +0200

    map views to URLs in templates to avoid hardcoded URLs

--- src/machines/urls.py
+++ src/machines/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 import machines.views
 
 urlpatterns = patterns('',
-    url(r'^$', machines.views.all_machines),
+    url(r'^$', machines.views.all_machines, name="all"),
     url(r'^/(?P<machine_name>[a-z0-9\-\.]+)$', machines.views.detail, name='detail'),
 )
+
