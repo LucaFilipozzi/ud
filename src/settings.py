c9a466c2aeedc55104efd71fa3be89f10c1597ce
Stéphane Blondon <stephane.blondon@gmail.com>
2016-08-08 23:23:22 +0200

    refactoring: os.path.join instead of concatenation; remove redefiniton of a constant

--- src/settings.py
+++ src/settings.py
@@ -102,13 +102,12 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'ud.urls'
-ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.getcwd() + "/templates",
+    os.path.join(os.getcwd(), "templates"),
 )
 
 INSTALLED_APPS = (
