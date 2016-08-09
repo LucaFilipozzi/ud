bf43c4bf340c7e209b2af39d35eb65a3fc0aedec
Stéphane Blondon <stephane.blondon@gmail.com>
2016-08-09 23:57:19 +0200

    text to display link instead of the URL when it's available

--- src/machines/templatetags/wiki_link.py
+++ src/machines/templatetags/wiki_link.py
@@ -1,26 +1,36 @@
 import re
 
 from django import template
-from django.template.defaultfilters import stringfilter, urlize
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter
 @stringfilter
 def wiki_link(value):
-    regexp = re.compile(r"""(?P<before>[a-z \.]*) # text before the url
+    regexp = re.compile(r"""(?P<before>[a-z \.]*) # text before the link
             \[\[                                 # [[
             [-\*]?                                # sometimes url starts with a '*' or '-'
-            (?P<url>[a-z0-9-\.]+)                 # url
+            (?P<link>[a-z0-9- |\.]+)              # url|text
             \]\]                                  # ]]
-            (?P<after>.*)                         # text after the url
+            (?P<after>.*)                         # text after the link
             """,
             re.VERBOSE)
     m = regexp.match(value)
     if m:
+        link = m.group("link").split("|")
+        url = link[0]
+        try:
+            text = link[1]
+        except IndexError:
+            text = url
         urlized = ''.join([
             m.group("before"),
-            urlize(m.group("url")),
+            "<a href=",
+            url,
+            ">",
+            text,
+            "</a>",
             m.group("after")
             ])
     else:
