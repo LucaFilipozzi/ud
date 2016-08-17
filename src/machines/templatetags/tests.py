524728a08ba3a61c8fb477fbeda6f61974de9390
Stéphane Blondon <stephane.blondon@gmail.com>
2016-08-17 20:00:21 +0200

    wiki_links pareses link with parenthesis in description

--- src/machines/templatetags/tests.py
+++ src/machines/templatetags/tests.py
@@ -12,6 +12,9 @@ class Test(unittest.TestCase):
     def test_link_with_description(self):
         self.assertEqual('<a href="http://url.com">interesting description</a>', wiki_link.wiki_links("[[url.com|interesting description]]"))
 
+    def test_link_with_parenthesis_in_description(self):
+        self.assertEqual('<a href="http://url.com">description (DESC)</a>', wiki_link.wiki_links("[[url.com|description (DESC)]]"))
+
     def test_link_start_with_star(self):
         self.assertEqual('<a href="http://url.com">url.com</a>', wiki_link.wiki_links("[[*url.com]]"))
 
@@ -21,6 +24,8 @@ class Test(unittest.TestCase):
     def test_several_links(self):
         self.assertEqual('<a href="http://url.com">url.com</a> <a href="http://other.org">other.org</a>', wiki_link.wiki_links("[[url.com]] [[other.org]]"))
 
+    def test_text_before_and_after_link(self):
+        self.assertEqual('hello <a href="http://url.com">url.com</a> bye bye', wiki_link.wiki_links("hello [[url.com]] bye bye"))
 
 
 if __name__ == '__main__':
